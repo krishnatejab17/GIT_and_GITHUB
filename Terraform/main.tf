eba6f2c172c33eb218229181562758134c0ec8d6
Krishnatejab17 <17bdevops@gmail.com>
2025-09-10 06:09:32 +0000

    terraform code for new ec2 instance

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,9 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "name" {
-  name = "new_ec2"
-  ami = ""
+resource "aws_instance" "new_ec2_from_terraform" {
+  ami = "ami-00ca32bbc84273381"
   instance_type = "t2.micro"
-  key_name = ""
+  key_name = "devops"
+  
+  tags = {
+    Name = "new_ec2"
+  }
 }
+
